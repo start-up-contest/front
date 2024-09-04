e9478abc16873a53dcad00a7c206237026f76aa4
byeori-moon <mblee2@naver.com>
2024-09-04 18:08:13 +0900

    FEAT: 로그인 UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -13,7 +13,6 @@ import right_arrow_mint from "../assets/icons/right_arrow_mint.svg";
 import guide from "../assets/guide.svg";
 import logo from "../assets/images/big_logo.png";
 
-
 function LoginPage() {
   const {} = useParams();
   const [content, setContent] = useState(null);
@@ -22,7 +21,20 @@ function LoginPage() {
     setContent(
       <div className={styles.container}>
         <img src={logo} className={styles.logo}></img>
-        <div className={styles.loginBox}></div>
+        <div className={styles.loginBox}>
+          <div className={styles.titleText} style={{marginBottom: "50px"}}>
+            다양한 서비스를 위해 로그인해주세요
+          </div>
+          <input className={styles.inputStyle} placeholder="이메일 주소 입력" />
+          <input className={styles.inputStyle} placeholder="비밀번호 입력" />
+          <button className={styles.loginButton}>로그인</button>
+          <div className={styles.bottomButton}>
+            <button className={styles.textButton}>계정 찾기</button>{" "}
+            <button className={styles.textButton}>비밀번호 찾기</button>
+            <div style={{flexGrow:"1"}}/>
+            <button className={styles.textButton}>회원가입</button>
+          </div>
+        </div>
       </div>
     );
   });
